6fadb380cb6a835dcb5eaea14e95e80bfaabcec9
naveen <neonew8984@gmail.com>
2021-11-14 22:04:36 +0530

    restful api using node with express and mongoose

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
+
+// Middlewares
+app.use(cors());
 
 app.use(express.json()); //Used to parse JSON bodies
 
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Post = require('../models/Post');
 // Routes
 
+// Get all posts
 router.get('/', async (req, res) => {
   try {
     const posts = await Post.find();
@@ -12,6 +13,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Post a Post I guess
 router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -24,6 +26,39 @@ router.post('/', async (req, res) => {
     res.json(savedPost);
   } catch (err) {
     res.json({ message: err });
+  }
+});
+
+//Retrieve a  Specific Post from Database
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json({ post });
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Delete a post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const deletedPost = await Post.remove({ _id: req.params.postId });
+    res.json({ deletedPost });
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Update a Post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (error) {
+    res.json({ message: error });
   }
 });
 
